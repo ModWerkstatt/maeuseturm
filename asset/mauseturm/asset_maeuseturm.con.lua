aba567618218044b682adcf99892d2505b1d0e7d
Dylan7654 <dylan.kreuzinger@gmail.com>
2026-04-11 21:45:03 +0200

    Mäuseturm fertig

--- asset/mauseturm/asset_maeuseturm.con.lua
+++ asset/mauseturm/asset_maeuseturm.con.lua
@@ -5,8 +5,8 @@ return {
 	description = { 
 		name = _("maeuseturm_asset"),			
 		description = _("maeuseturm_asset_desc"),
-		icon = "icons/raptorark_menu_fs_e636_smallIcon@2x.tga",
-		previewIcon = "",
+		icon = "icons/assets_maeuseturm_smallIcon@2x.tga",
+		previewIcon = "icons/assets_maeuseturm_preview@2x.tga",
 	},
 	menuCategory = {
 		categories = {
@@ -28,10 +28,10 @@ return {
 	params = {
 		{
 			key = "maeuseturm",
-			name = _("maeuseturm_asset"),
+			name = _("maeuseturm_menu"),
 			uiType = "IconButton",
 			values = { "icons/mauseturm_alt_icon20@2x.tga", "icons/mauseturm_medium_icon20@2x.tga", "icons/mauseturm_modern_icon20@2x.tga", },
-			tooltip = _("tooltip_assets"),
+			tooltip = _("tooltip_maeuseturm"),
 			defaultIndex = 1,
 		},
 	},
